bc651d3a8aee64b2ecf618c2731b16c6a3bee87a
Nehali kumari <nehalijha2306@gmail.com>
2026-06-07 09:27:01 +0530

    Added Even or Odd number checker program

---
+++ even_odd.py
@@ -0,0 +1,10 @@
+# Program: Even or Odd Checker
+# Author : Nehali
+# Language : Python
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+     print(num, "is an even number")
+else:
+     print(num, "is an odd number")
+     
